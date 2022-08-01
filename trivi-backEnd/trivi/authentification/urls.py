268ea42438da1aac321cf7936ae3f9c1262e9fd0
Minh Triet TO <minhtriet.to29@gmail.com>
2022-08-01 16:11:24 -0400

    Delete trivi-backEnd/trivi directory

--- trivi-backEnd/trivi/authentification/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import current_user, UserList, change_password
-
-urlpatterns = [
-    path('change-password/', change_password),
-    path('current_user/', current_user),
-    path('users/', UserList.as_view())
-]
